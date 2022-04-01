f9e9698f280e882a368adc6b14fd7504eb3df28f
Yihua Zhang <yihuazhang2@gmail.com>
2022-03-31 23:29:58 -0400

    using query with variable

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,21 +1,42 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
 
 import ProductCard from '../../components/product-card/product-card.component';
 import Spinner from '../../components/spinner/spinner.component';
 
-import { CategoriesContext } from '../../contexts/categories.context';
-
 import { CategoryContainer, Title } from './category.styles';
+
+const GET_CATEGORY = gql`
+  query ($title: String) {
+    getCollectionsByTitle(title: $title) {
+      title
+      id
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: { title: category },
+  });
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+      setProducts(items);
+    }
+  }, [category, data]);
 
   return (
     <Fragment>
