633640f36d3fb20bcc01c56fc72cdec475bb6fb3
bmmukhridin <bmmukhridin@icloud.com>
2022-10-15 12:28:56 -0700

    working/with/graplQL

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,22 +1,30 @@
-import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { createContext, useState, useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query GetCollections {
+    collections {
+      id,
+      title,
+      items{
+        id,
+        name,
+        price,
+        imageUrl
+      }
+    }
+  }
+`
+
 export const CategoriesProvider = ({ children }) => {
+  const {loading, error, data} = useQuery(COLLECTIONS)
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
+  console.log(data)
 
   const value = { categoriesMap };
   return (
