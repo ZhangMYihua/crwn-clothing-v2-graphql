24e2646164548e3ffdd3636f011a2fc9bbfa6811
Pieter Deane <deanepieter@gmail.com>
2024-02-12 06:53:28 +0200

    integrate GraphQL with Apollo Client and implement categories query

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
@@ -6,22 +7,28 @@ export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query GetCollections {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
+  console.log('loading...', loading);
+  console.log('data', data);
 
   const value = { categoriesMap };
-  return (
-    <CategoriesContext.Provider value={value}>
-      {children}
-    </CategoriesContext.Provider>
-  );
+  return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>;
 };
