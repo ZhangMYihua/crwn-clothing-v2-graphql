c8f552b7991fb9c4f0d820eacdf90fd2681379cc
Pieter Deane <deanepieter@gmail.com>
2024-02-13 06:50:56 +0200

    Add variables to useQuery for enhanced data fetching

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,22 +1,41 @@
 import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
-
+import { gql, useQuery } from '@apollo/client';
 import ProductCard from '../../components/product-card/product-card.component';
-
-import { CategoriesContext } from '../../contexts/categories.context';
-
 import { CategoryContainer, Title } from './category.styles';
 import Spinner from '../../components/spinner/spinner.component';
 
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: { title: category },
+  });
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+      setProducts(items);
+    }
+  }, [category, data]);
 
+  const [products, setProducts] = useState();
   return (
     <Fragment>
       {loading ? (
